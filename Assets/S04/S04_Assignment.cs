6e56a65b6d586302fb9ee45c9b4b83f6687937cc
jiyeon <sueandcream@ewhain.net>
2026-09-13 15:15:46 +0900

    Add vectors

--- Assets/S04/S04_Assignment.cs
+++ Assets/S04/S04_Assignment.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
 public class S04_Assignment : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f), // 0
+            new Vector3(1f, 0f, 0f), // 1
+            new Vector3(1f, 1f, 0f), // 2
+            new Vector3(0f, 1f, 0f), // 3
+            new Vector3(0f, 0f, 1f), // 4
+            new Vector3(1f, 0f, 1f), // 5
+            new Vector3(1f, 1f, 1f), // 6
+            new Vector3(0f, 1f, 1f), // 7
+        };
     }
 
     // Update is called once per frame
