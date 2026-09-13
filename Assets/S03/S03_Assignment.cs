15ccec7b86b2a7f17fe74ba096c464f7568591c5
jiyeon <sueandcream@ewhain.net>
2026-09-13 15:07:46 +0900

    Add vectors

--- Assets/S03/S03_Assignment.cs
+++ Assets/S03/S03_Assignment.cs
@@ -5,7 +5,18 @@ public class S03_Assignment : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f), // 0
+            new Vector3(1f, 0f, 0f), // 1
+            new Vector3(1f, 0.5f, 1f), // 2
+            new Vector3(0.5f, -0.5f, 1f), // 3
+            new Vector3(0f, 0f, 1f), // 4
+            new Vector3(1f, 0f, 1f), // 5
+            new Vector3(0.5f, 1f, 1f), // 6
+            new Vector3(0f, 0.5f, 1f), // 7
+        };
+
     }
 
     // Update is called once per frame
